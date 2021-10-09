482806a2027d6b45fb4c8697f2c22873d7f07216
Jlew112 <87906387+Jlew112@users.noreply.github.com>
2021-10-09 09:37:11 -0600

    Add files via upload

---
+++ WeatherPy/Resources/OpenWeatherAPI.py
@@ -0,0 +1,2 @@
+#API key. account jlew112 ///// !9#?4&r2QdwLsR?
+6332b42b6f825758ae6fa5f871054c38
---
+++ WeatherPy/Weather_Database/config.py
@@ -0,0 +1,3 @@
+#API key. Open weather account jlew112 ///// !9#?4&r2QdwLsR?
+weather_api_key = "6332b42b6f825758ae6fa5f871054c38"
+g_key = "AIzaSyCgmvwUMcaTgJiUE-ZbbDolTzw2FZooiy0"
